914f8a7e67ada23927a443795773e6a7accdc577
Chinedu Okorie <okoriechinedusunday@gmail.com>
2023-03-11 20:30:25 +0100

    update status codes

--- utils/response.js
+++ utils/response.js
@@ -1,12 +1,14 @@
-exports.responseText={
-    "FAIL":"FAIL",
-    "SUCCESS":"SUCCESS"
-}
+exports.responseText = {
+  FAIL: "FAIL",
+  SUCCESS: "SUCCESS",
+};
 
-exports.statusCodes={
-    "200":200,
-    "400":400,
-    "401":401,
-    "403":403,
-    "500":500
-}
+exports.statusCodes = {
+  200: 200,
+  201: 201,
+  400: 400,
+  404: 404,
+  401: 401,
+  403: 403,
+  500: 500,
+};
