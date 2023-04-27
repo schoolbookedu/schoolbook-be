e160cbae60f8f4136904d50bc3a0c827985323d2
seghodavid <56871763+seghodavid@users.noreply.github.com>
2023-04-27 12:49:30 +0100

    Course: Fixed issues

--- controllers/course.js
+++ controllers/course.js
@@ -1,5 +1,3 @@
-const { Types } = require("mongoose");
-const { ObjectId } = Types;
 const {
   getOne,
   getAll,
--- routes/course.js
+++ routes/course.js
@@ -7,14 +7,14 @@ const {
   deleteCourse,
 } = require("../controllers/course");
 const { authenticate } = require("../middlewares/auth");
-const { CourseCreationValidation } = require("../validations/course.validation");
+const { CourseCreationValidation, CourseUpdateValidation } = require("../validations/course.validation");
 
 const courseRouter = express.Router();
 
 courseRouter.get("/", authenticate, getAllCourse);
 courseRouter.get("/:id", authenticate, getACourse);
 courseRouter.post("/", CourseCreationValidation, createCourse);
-courseRouter.patch("/", UserUpdateValidation, updateCourse);
+courseRouter.patch("/", CourseUpdateValidation, updateCourse);
 courseRouter.delete("/:id", authenticate, deleteCourse);
 
 module.exports = { courseRouter };
